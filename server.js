e38c336d855c1b5be005ffc474a1614e836fc718
Ivan <ku353048@gmail.com>
2024-03-28 19:54:11 +0200

    ballons_game

---
+++ server.js
@@ -0,0 +1,14 @@
+import express, { json } from "express"
+import {appendFileSync} from 'fs'
+const app = express()
+
+
+
+app.use(express.static('./'))
+app.use(express.json())
+
+app.post('/stats', (req, res) => {
+    appendFileSync('statistics.txt', JSON.stringify(req.body) + '\n')
+})
+
+app.listen(3000)
